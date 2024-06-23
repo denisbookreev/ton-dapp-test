305e3b38fca9b7f6169373f3ede75121d573e8d7
denisbookreev <denisbookreev@evrone.com>
2024-06-23 18:56:41 +0700

    Workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
 
-const manifestUrl = 'http://localhost:5173/manifest.json'
+const manifestUrl = 'https://denisbookreev.github.io/ton-dapp-test/tonconnect-manifest.json'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
